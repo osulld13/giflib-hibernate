eb337c7bdff5f7fb2cb1b970338d666854f514bd
dosullivan_gw <dosullivan@guidewire.com>
2020-02-17 17:07:26 +0000

    Added properties and data config

---
+++ src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
@@ -0,0 +1,27 @@
+package com.teamtreehouse.giflib.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@PropertySource("app.properties")
+public class DataConfig {
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        Resource config = new ClassPathResource("hibernate.cfg.xml");
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setConfigLocation(config);
+        // Allows a package to be passed in that contains the JPA annotated entity classes
+        sessionFactory.setPackagesToScan(env.getProperty("giflib.entities.package"));
+        return sessionFactory;
+    }
+}
