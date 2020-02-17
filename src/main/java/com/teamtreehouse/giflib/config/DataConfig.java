1700727870e9e9e3e99cc817ecd0894a798743f5
osulld13 <osulld13@tcd.ie>
2020-02-17 19:35:47 +0000

    Added data config

--- src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
+++ src/main/java/com/teamtreehouse/giflib/config/DataConfig.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse.giflib.config;
 
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("app.properties")
@@ -22,6 +25,20 @@ public class DataConfig {
         sessionFactory.setConfigLocation(config);
         // Allows a package to be passed in that contains the JPA annotated entity classes
         sessionFactory.setPackagesToScan(env.getProperty("giflib.entities.package"));
+        sessionFactory.setDataSource(dataSource());
         return sessionFactory;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource ds = new BasicDataSource();
+        // Driver class name
+        ds.setDriverClassName(env.getProperty("giflib.db.driver"));
+        // URL
+        ds.setUrl(env.getProperty("giflib.db.url"));
+        // Username and Password
+        ds.setUsername(env.getProperty("giflib.db.username"));
+        ds.setPassword(env.getProperty("giflib.db.password"));
+        return ds;
     }
 }
